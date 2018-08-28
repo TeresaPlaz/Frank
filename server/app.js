af5cdbb060e706d10ca5b9eee1e3dd4259fc9e8b
Teresa Plaz <teresaplaz@gmail.com>
2018-08-27 23:12:20 -0400

    added schemas

--- server/app.js
+++ server/app.js
@@ -4,27 +4,42 @@ const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
 const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors');
+const session      = require('express-session');
+const passport     = require('passport');
+
+
 
 
 mongoose.Promise = Promise;
 mongoose
-  .connect('mongodb://localhost/server', {useMongoClient: true})
+  .connect('mongodb://localhost/Frank', {useMongoClient: true})
   .then(() => {
-    console.log('Connected to Mongo!')
+    console.log( 'Connected to Mongo!' );
   }).catch(err => {
-    console.error('Error connecting to mongo', err)
+    console.error( 'Error connecting to mongo', err );
   });
 
 const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const debug = require( 'debug' )( `${app_name}:${path.basename( __filename ).split( '.' )[ 0 ]}` );
+const passportSetup = require('./configs/passport');
+passportSetup(passport);
 
 const app = express();
 
 // Middleware Setup
+app.use(session({
+  secret: 'angular auth passport secret shh',
+  resave: true,
+  saveUninitialized: true,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -37,10 +52,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
