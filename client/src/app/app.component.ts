059c13e3d1089ad69f598433c9ab9000222ebfd2
Teresa Plaz <teresaplaz@gmail.com>
2018-09-08 22:06:42 -0400

    urikitaka

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthLogService } from './franki-home/service/auth-log.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
-export class AppComponent
-{
-  /**
-   *
-   */
-  constructor(private authControlLog: AuthLogService) { }
-  title = 'client';
+} )
+  
+export class AppComponent implements OnInit {
+    constructor(private authControlLog: AuthLogService) {}
+
+    ngOnInit() {
+        //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+        //Add 'implements OnInit' to the class.
+
+    }
+     isLoggedIn2 = this.authControlLog.isLoggedIn().subscribe(user => { return ; }, err => { console.error(err) });
 }
