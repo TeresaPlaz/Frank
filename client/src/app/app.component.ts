e9cf08de58cd6d4f17612c8c7280548ab2534860
Teresa Plaz <teresaplaz@gmail.com>
2018-09-08 21:26:02 -0400

    front end

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthLogService } from './franki-home/service/auth-log.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent
+{
+  /**
+   *
+   */
+  constructor(private authControlLog: AuthLogService) { }
   title = 'client';
 }
