62812dd218570860c2cd1bb5dbc30f0e68cb07bb
ArielPrez <arielpgmm@gmail.com>
2018-09-09 19:26:03 -0400

    angular background video, navbar transparent, logForm pretty and center

--- client/src/app/franki-home/franki-home.component.ts
+++ client/src/app/franki-home/franki-home.component.ts
@@ -11,6 +11,12 @@ export class FrankiHomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const video = document.getElementsByTagName("video")[0];
+
+  // video.width = Math.floor(window.innerWidth);
+  // video.style.width = video.width+"px";
+  video.height = Math.floor(window.innerHeight) * 1.4;
+  video.style.height = video.height+"px";
   }
 
   // video = document.getElementsByTagName("video")[0];
